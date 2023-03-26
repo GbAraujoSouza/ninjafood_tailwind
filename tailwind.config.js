16de5d5f95a4f57e70005939b19d847bc60ea10c
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2023-03-25 22:41:25 -0300

    formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // eslint-disable-next-line quotes
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
